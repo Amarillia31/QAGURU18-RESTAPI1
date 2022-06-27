1f548285bdce2867b4e77df033f85e32e3e9a5b0
elena <elena.doronina@exactpro.com>
2022-06-28 03:28:45 +0400

    test push

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,49 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import io.restassured.RestAssured;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import config.ProjectConfig;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+
+public class TestBase {
+
+    @BeforeAll
+    static void setUp() {
+        ProjectConfig config = ConfigFactory.create(ProjectConfig.class,System.getProperties());
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+
+        Configuration.baseUrl = config.getBaseUrl();
+        RestAssured.baseURI = config.getBaseUri();
+//        Configuration.remote = config.getRemoteUrl();
+
+        Configuration.browser = config.getBrowser();
+        Configuration.browserSize = config.getBrowserSize();
+        Configuration.browserVersion = config.getBrowserVersion();
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+
+    }
+    @AfterEach
+    void afterEach() {
+        Attach.screenshotAs("Скриншот выполненного теста");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
+    }
+}
